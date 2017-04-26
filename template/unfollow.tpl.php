8c703f20824e4fe5624118a07f3e984130e9925e
evelyn_zhang <1054982653@qq.com>
2017-04-26 14:58:06 +0800

    unfollow

--- template/unfollow.tpl.php
+++ template/unfollow.tpl.php
@@ -9,8 +9,9 @@
     <meta name="full-screen" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
     <link rel="stylesheet" type="text/css" href="../src/dist/css/style.css"/>
-    <script src="../src/assets/js/rem.js"></script>
+    <script src="http://kenzowechat.samesamechina.com/weixin/jssdkforsite?debug=1"></script>
     <script src="../src/assets/js/lib/zepto.min.js"></script>
+    <script src="../src/assets/js/rem.js"></script>
     <script src="../src/assets/js/common.js"></script>
     <script src="../src/assets/js/wxshare.js"></script>
 </head>
